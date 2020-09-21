773762297b169e73fde0e7148d1009f7c9da34d6
William <williamdiass@outlook.com>
2020-09-21 14:58:44 -0300

    Webhook history

--- resources/views/webhook/history.blade.php
+++ resources/views/webhook/history.blade.php
@@ -1,58 +1,107 @@
-<script>document.getElementsByTagName("html")[0].className += " js";</script>
-<link rel="stylesheet" href="/vendor/digisac/core-integration-laravel/css/timeline.css">
-
-
-<section class="cd-h-timeline js-cd-h-timeline margin-bottom-md">
-    <div class="cd-h-timeline__container container">
-        <div class="cd-h-timeline__dates">
-            <div class="cd-h-timeline__line">
-                <ol>
-                    <?php if(count($trace_requests)){
-                        foreach($trace_requests as $TraceRequest){ ?>
-
-                        <li><a href="#0" data-date="{{$TraceRequest->created_at}}" class="cd-h-timeline__date">{{$TraceRequest->type}}</a></li>
-
-                 <?php } } ?>
-                </ol>
-
-                <span class="cd-h-timeline__filling-line" aria-hidden="true"></span>
-            </div> <!-- .cd-h-timeline__line -->
-        </div> <!-- .cd-h-timeline__dates -->
-
-        <ul>
-            <li><a href="#0" class="text-replace cd-h-timeline__navigation cd-h-timeline__navigation--prev cd-h-timeline__navigation--inactive">Prev</a></li>
-            <li><a href="#0" class="text-replace cd-h-timeline__navigation cd-h-timeline__navigation--next">Next</a></li>
-        </ul>
-    </div> <!-- .cd-h-timeline__container -->
-
-    <div class="cd-h-timeline__events">
-        <ol>
+<section class="time-line-box">
+    <div class="swiper-container text-center">
+        <div class="swiper-wrapper">
             <?php if(count($trace_requests)){
-                foreach($trace_requests as $TraceRequest){ ?>
-            <li class="cd-h-timeline__event cd-h-timeline__event--selected text-component">
-                <div class="cd-h-timeline__event-content container">
-                    <h2 class="cd-h-timeline__event-title">{{$TraceRequest->type}}</h2>
-                    <em class="cd-h-timeline__event-date">{{$TraceRequest->created_at}}</em>
-                    <p class="cd-h-timeline__event-description color-contrast-medium">
-                        <code>
-                            {
-                            "event": "bot.command",
-                            "data": {
-                            "id": "11fb65fb-9e24-4dd0-aa53-e8b4cecb47f4",
-                            "contactId": "d85ae406-b6d2-473e-8983-4379f96463e1",
-                            "accountId": "e57575aa-8995-43b6-8942-31e6e9135a25",
-                            "command": "insere_nome_cancelamento",
-                            "message": {
-                            "id": "96b9dc15-d8a0-453c-bf14-cddf0c49445d",
-                            }
-                        </code>
-                    </p>
-                </div>
-            </li>
-                <?php } } ?>
-        </ol>
-    </div> <!-- .cd-h-timeline__events -->
+            foreach($trace_requests as $TraceRequest){
+            $date = new \DateTime($TraceRequest->created_at);
+            ?>
+            <div class="swiper-slide">
+                <div class="timestamp"><span class="date"><i
+                                class="fa fa-clock-o"></i> {{$date->format('d/m/Y H:i')}}</span></div>
+                <div class="status"><span> {{$TraceRequest->type}}</span></div>
+            </div>
+            <?php } }  ?>
+
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
 </section>
-<script src="/vendor/digisac/core-integration-laravel/js/util.js"></script> <!-- util functions included in the CodyHouse framework -->
-<script src="/vendor/digisac/core-integration-laravel/js/swipe-content.js"></script> <!-- A Vanilla JavaScript plugin to detect touch interactions -->
-<script src="/vendor/digisac/core-integration-laravel/js/timeline.js"></script>
+
+<style>
+    .timestamp .date {
+        font-size: 13px;
+        font-weight: bolder;
+        padding-bottom: 15px !important;
+    }
+
+    .status span {
+        color: #4e70fc;
+        font-size: 15px;
+        font-weight: bolder;
+        padding-top: 25px !important;
+    }
+
+    .time-line-box {
+        height: 100px;
+        padding: 25px 0;
+        width: 100%;
+        height: 195px;
+    }
+
+    .time-line-box .timeline {
+        list-style-type: none;
+        display: flex;
+        padding: 0;
+        text-align: center;
+    }
+
+    .time-line-box .timestamp {
+        margin: auto;
+        margin-bottom: 5px;
+        padding: 0px 4px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .time-line-box .status {
+        padding: 0px 10px;
+        display: flex;
+        justify-content: center;
+        border-top: 3px solid #455EFC;
+        position: relative;
+        transition: all 200ms ease-in;
+    }
+
+    .time-line-box .status span {
+        padding-top: 8px;
+    }
+
+    .time-line-box .status span:before {
+        content: '';
+        width: 12px;
+        height: 12px;
+        background-color: #455EFC;
+        border-radius: 12px;
+        border: 2px solid #455EFC;
+        position: absolute;
+        left: 50%;
+        top: 0%;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        transition: all 200ms ease-in;
+    }
+
+    .swiper-container {
+        width: 95%;
+        margin: auto;
+        overflow-y: auto;
+    }
+
+    .swiper-wrapper {
+        display: inline-flex;
+        flex-direction: row;
+        overflow-y: auto;
+        justify-content: center;
+    }
+
+    .swiper-slide {
+        margin-bottom: 35px;
+        text-align: center;
+        font-size: 12px;
+        width: 200px;
+        height: 100%;
+        position: relative;
+    }
+</style>
--- src/DigiSacCoreIntegrationServiceProvider.php
+++ src/DigiSacCoreIntegrationServiceProvider.php
@@ -20,7 +20,7 @@ class DigiSacCoreIntegrationServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-	     // \URL::forceScheme('https');
+	     \URL::forceScheme('https');
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
